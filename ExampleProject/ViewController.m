d7213d6bdae0f86085dd6efb4ec55332bc465c9a
Philippe Faucon <pfaucon@asu.edu>
2015-03-24 10:59:41 -0700

    Created base project.  Note auto layout constraint on the tableview has a top gap of 20 points, this is because the top status bar (on all platforms) is 20 points high.  The "top" of your view controller falls under the status bar unless you have a nav controller.

--- ExampleProject/ViewController.m
+++ ExampleProject/ViewController.m
@@ -10,6 +10,7 @@
 
 @interface ViewController ()
 
+@property NSArray *nameList;
 @end
 
 @implementation ViewController
@@ -17,11 +18,40 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    self.nameList = @[@"Christophe",@"John"];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+#pragma mark - UITableViewDataSource
+
+-(NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    return 1;
+}
+-(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return self.nameList.count;
+}
+-(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"cell"];
+    
+    cell.textLabel.text = self.nameList[indexPath.row];
+    
+    return cell;
+}
+
+#pragma mark - UITableViewDelegate
+
+-(NSString *)tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section
+{
+    return @"Awesome People";
+}
+
 
 @end

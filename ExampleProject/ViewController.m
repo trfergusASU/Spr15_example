d61e9ce934782c9b4879ccd519ae560e7183ceaf
sethwood <sethheywood22@gmail.com>
2015-04-03 16:08:51 -0700

    Seth Heywood added name

--- ExampleProject/ViewController.m
+++ ExampleProject/ViewController.m
@@ -19,7 +19,7 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     
-    self.nameList = @[@"Christophe",@"John",@"Michael Welker",@"Ted"];
+    self.nameList = @[@"Christophe",@"John",@"Michael Welker",@"Ted", @"Seth Heywood"];
 }
 
 - (void)didReceiveMemoryWarning {

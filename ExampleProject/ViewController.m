0ce68b598bbe11c577ecf81776e76512c74546ba
arhiggi3 <arhiggi3@asu.edu>
2015-04-15 14:01:17 -0700

    added Andrew Higgins

--- ExampleProject/ViewController.m
+++ ExampleProject/ViewController.m
@@ -19,7 +19,7 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     
-    self.nameList = @[@"Christophe",@"John",@"Michael Welker",@"Ted", @"Seth Heywood", @"John Radcliffe"];
+    self.nameList = @[@"Christophe",@"John",@"Michael Welker",@"Ted", @"Seth Heywood", @"John Radcliffe", @"Andrew Higgins"];
 }
 
 - (void)didReceiveMemoryWarning {

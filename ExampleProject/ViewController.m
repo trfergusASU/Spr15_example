260d8009214d3023636e93615d8bf1d05b417a41
trfergusASU <trfergus@asu.edu>
2015-04-16 12:29:57 -0700

    Added name Tim Ferguson
    Added name - Tim Ferguson

--- ExampleProject/ViewController.m
+++ ExampleProject/ViewController.m
@@ -19,7 +19,7 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     
-    self.nameList = @[@"Christophe",@"John",@"Michael Welker",@"Ted", @"Seth Heywood", @"John Radcliffe", @"Andrew Higgins", @"Miguel Lopez", @"Marcelo Sedano"];
+    self.nameList = @[@"Christophe",@"John",@"Michael Welker",@"Ted", @"Seth Heywood", @"John Radcliffe", @"Andrew Higgins", @"Miguel Lopez", @"Marcelo Sedano", @"Tim Ferguson"];
 }
 
 - (void)didReceiveMemoryWarning {
